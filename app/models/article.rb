091786e4b2c3c8754992049ecdff49852c4e3fbd
Sid Shroff <shroff.siddharth@gmail.com>
2016-09-06 05:40:07 +0000

    moved to using minimum, maximum

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { in: 3..10 }
-  validates :description, presence: true, length: { in: 10..300 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
