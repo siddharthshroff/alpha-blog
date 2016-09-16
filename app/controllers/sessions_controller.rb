5da387c5ae325a844f12ca5d3670ff3bd2214e7b
Sid Shroff <shroff.siddharth@gmail.com>
2016-09-16 07:42:06 +0000

    Fix tests and update nav with categories

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have logged out"
-    redirect_to root_path
+    redirect_to login_path
   end
 end
