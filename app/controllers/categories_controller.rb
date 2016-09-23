e7b2ee809e7cec031b5a215c8c74a66f062c6036
Sid Shroff <shroff.siddharth@gmail.com>
2016-09-23 07:02:38 +0000

    Add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show]
   
   def new 
     @category = Category.new
@@ -15,13 +16,25 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+  
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
   
   
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -34,9 +47,13 @@ class CategoriesController < ApplicationController
   
   def require_admin
     if !logged_in? || (logged_in? && !current_user.admin?)
-      flash[:danger] = "You cannot add new categories"
+      flash[:danger] = "You cannot add or edit categories"
       redirect_to categories_path
     end
+  end
+  
+  def set_category
+    @category = Category.find(params[:id])
   end
 
 end 
